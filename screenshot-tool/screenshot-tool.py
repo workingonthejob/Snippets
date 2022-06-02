e29ab4b086f1b754f63c8cfbae651cc52aab5ef8
workingonthejob <cs.masj@gmail.com>
2022-06-02 11:15:53 -0400

    Cleaned up screenshot tool to use argparse and better handle paths.

--- screenshot-tool/screenshot-tool.py
+++ screenshot-tool/screenshot-tool.py
@@ -1,44 +1,66 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import argparse
 import sys
 import time
 
-
-# from selenium.webdriver.common.action_chains import ActionChains
-# actions = ActionChains(driver)
-# actions.move_to_element(deck).perform()
+# Info on headless maximized windows
+# https://stackoverflow.com/questions/45374377/not-able-to-maximize-chrome-window-in-headless-mode
 
 X_NO_THANKS_BTN = '//button[@class="decline-button eu-cookie-compliance-default-button"]'
-X_PLAYER = './/h4'
-
-options = Options()
-
-#options.headless = True
-options.add_argument("--start-maximized")
-
-driver = webdriver.Chrome(options=options)
+X_PLAYERS = './/h4'
+X_HEADER = '//header'
 
 
-try:
-    driver.get('http://magic.wizards.com/en/articles/archive/mtgo-standings/modern-league-2022-05-27')
-    time.sleep(5)
-    elm = driver.find_element(by=By.XPATH, value=X_NO_THANKS_BTN)
-    elm.click()
-    time.sleep(5)
-    decks = driver.find_elements(by=By.XPATH, value='//div[@class="deck-group"]')
-    names = driver.find_elements(by=By.XPATH, value=X_PLAYER)
+class ScreenshotTool():
+    def __init__(self, url, output_dir):
+        self.output_dir = output_dir
+        self.url = url
 
-    for i in range(len(decks)):
-        player_name = names[i].text.split(" ")[0]
-        print("{}[{}]".format(player_name, i))
-        decks[i].location_once_scrolled_into_view
-        time.sleep(1)
-        decks[i].screenshot("{}.png".format(player_name))
+    def run(self):
+        driver.get(self.url)
+        time.sleep(2)
+        driver.execute_script('document.querySelector("header").style.display = "none"')
+        no_thanks_btn_elm = driver.find_element(by=By.XPATH, value=X_NO_THANKS_BTN)
+        no_thanks_btn_elm.click()
+        time.sleep(2)
+        decks = driver.find_elements(by=By.XPATH, value='//div[@class="deck-group"]')
+        names = driver.find_elements(by=By.XPATH, value=X_PLAYERS)
+        number_of_decks = len(decks)
 
-    #print(dir(elm))
-    #print(dir(driver))
-except Exception as e:
-    print(e)
-finally:
-    driver.close()
+        for i in range(number_of_decks):
+            player_name = names[i].text.split(" ")[0]
+            print("{}[{}/{}]".format(player_name, i + 1, number_of_decks))
+            decks[i].location_once_scrolled_into_view
+            time.sleep(1)
+            decks[i].screenshot(r"{}\{}.png".format(self.output_dir, player_name))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+    parser.add_argument("-d","--directory", help="The directory to save screenshots to.", default=r".\"")
+    parser.add_argument("-u","--url", help="The page to start at or screenshot.", required=True)
+    args = parser.parse_args()
+
+    # Define the options we want
+    # More options here:
+    # https://peter.sh/experiments/chromium-command-line-switches/
+    options = Options()
+    options.add_argument("--headless")
+    # Starting maximized headless doesn't work correctly.
+    options.add_argument("--window-size=1920x1080")
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        tool = ScreenshotTool(args.url, r"{}".format(args.directory))
+        tool.run()
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        driver.quit()
